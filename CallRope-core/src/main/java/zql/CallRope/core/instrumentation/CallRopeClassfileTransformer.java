a5e27ac14e3d5df766db09ac8983e700fd38043f
DESKTOP-D82NC11\24982 <2498298782@qq.com>
2024-04-25 22:09:40 +0800

    测试适配controller层

--- CallRope-core/src/main/java/zql/CallRope/core/instrumentation/CallRopeClassfileTransformer.java
+++ CallRope-core/src/main/java/zql/CallRope/core/instrumentation/CallRopeClassfileTransformer.java
@@ -1,11 +1,14 @@
 package zql.CallRope.core.instrumentation;
 
+import javassist.*;
 import zql.CallRope.core.adaptor.ClassAdaptor;
 import zql.CallRope.core.adaptor.adaptorImpl.HttpMethodImplEnum;
 import zql.CallRope.core.aspect.SpyImpl;
 
+import java.io.IOException;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
+import java.security.MessageDigest;
 import java.security.ProtectionDomain;
 import static zql.CallRope.core.adaptor.ClassAdaptor.supportClassMap;
 
@@ -22,19 +25,24 @@ public class CallRopeClassfileTransformer implements ClassFileTransformer {
     @Override
     public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined,
                             ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
-        if(className.contains("zql/CallRope/springBootDemo/controller")){
-            System.out.println(loader);
-            System.out.println(className);
-        }
-        if(!className.equals("org/springframework/boot/loader/LaunchedURLClassLoader")){
-            return classfileBuffer;
-        }
-        System.out.println("---------------------------------------------------------------------------------------");
-        System.out.println("transform:" + className);
-        System.out.println("---------------------------------------------------------------------------------------");
-        if(className.equals("org/springframework/boot/loader/LaunchedURLClassLoader")){
-            System.out.println(loader);
-            return HttpMethodImplEnum.SpringBootAdaptorImpl.modifyClass(className,classfileBuffer,SPY_JAR_PATH);
+        try{
+            if(className.contains("zql/CallRope/springBootDemo/controller")){
+                System.out.println(loader);
+                System.out.println(className);
+                ClassPool classPool = ClassPool.getDefault();
+                classPool.appendClassPath(new LoaderClassPath(loader));
+                CtClass ctClass = classPool.get("zql.CallRope.springBootDemo.controller.loginController");
+                CtMethod[] declaredMethods = ctClass.getDeclaredMethods();
+                for(CtMethod ctMethod : declaredMethods){
+                    ctMethod.insertAfter("System.out.println(\"12345\");");
+                }
+                ctClass.toClass(loader,ctClass.getClass().getProtectionDomain());
+                return null;
+            }
+        }catch (NotFoundException e) {
+            e.printStackTrace();
+        } catch (CannotCompileException e) {
+            e.printStackTrace();
         }
         return classfileBuffer;
     }
---
+++ CallRope-http-example/src/main/java/zql/CallRope/springBootDemo/controller/loginController.java
@@ -0,0 +1,22 @@
+package zql.CallRope.springBootDemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class loginController {
+
+    @GetMapping("/test/user/zql/114514")
+    public String login(){
+        try {
+            System.out.println("运行了login方法");
+            Thread.sleep(3000l);
+            System.out.println("睡了3秒");
+            return "hello zql";
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "hello zql";
+    }
+
+}
